8939080aad0f007f53848918e87b95b160eecbef
dro20dayethereumchallenge <dro20dayethereumchallenge@gmail.com>
2024-07-30 13:48:26 -0400

    Update Hello.js

--- test/Hello.js
+++ test/Hello.js
@@ -1,8 +1,9 @@
 const { expect } = require("chai");
 
-describe("Hello", function () {
-  it("Contract has been deployed successfully", async function () {
 
+
+describe("Hello deployment", function () {
+  it("Contract has been deployed successfully", async function () {
     const hardhatHello = await ethers.deployContract("Hello");
     expect(hardhatHello , "contract has been deployed");
   });
@@ -12,29 +13,24 @@ describe("Hello variable test", function() {
   it("contract variable set appropriately", async function() {
     const hardhatHello  = await ethers.deployContract("Hello");
     expect(await hardhatHello.storedData()).to.equal("Hello, World");
- 
-
   });
 });
 
 
 describe("owner()", function(){
   it("returns the address of the owner", async function(){
-	  const hardhatHello = await ethers.deployContract("Hello");
-	  const contractAddress = await hardhatHello.address;
-	  expect(contractAddress, "The current owner");
+          const hardhatHello = await ethers.deployContract("Hello");
+          const contractAddress = await hardhatHello.address;
           const [deployer] = await ethers.getSigners();
-	  const owners = await ethers.getSigners();
-	  //expect(deployer).to.equal(owner);
-          expect(deployer,"The current deployer");
+          const owners = await ethers.getSigners();
+          console.log("deployer", deployer);
+          console.log("deployer.address",deployer.address);
+          console.log("owners[0]", owners[0]);
+          console.log("owners[0]".address, owners[0].address);
           expect(deployer).to.equal(deployer);
           expect(deployer.address).to.equal(hardhatHello.runner.address);
-
   });
 });
-
-
-
 describe("set hello!", function(){
   it("test my set function", async function(){
           const hardhatHello = await ethers.deployContract("Hello");
@@ -44,25 +40,19 @@ describe("set hello!", function(){
 
 });
 
-
-
 describe("set hello!", function(){
   it("test my set function", async function(){
           const hardhatHello = await ethers.deployContract("Hello");
           const owners = await ethers.getSigners();
-
-	  try {
-	  await hardhatHello.set("Hello Again",{ from: owners[1] } );
-  }
-	catch(err){
-			const errM = "caller is not owner"
-	  expect(err).to.equal(errM)
-return;
-	} 
-
-
+          try {
+              await hardhatHello.set("Hello Again",{ from: owners[0] } );
+          }
+          catch(err){
+                const errM = "caller is not owner"
+                expect(err).to.equal(errM)
+                return;
+          }
   });
-
 });
 
 
@@ -73,33 +63,14 @@ describe("set hello! the right way", function(){
           const owners = await ethers.getSigners();
 
           try {
-          await hardhatHello.set("Hello Again",{ from: owners[0] } );
-  }
-        catch(err){
-                        const errM = "caller is not owner"
-          expect(err).to.equal(errM)
-return;
-        }
-
-
+              await hardhatHello.set("Hello Again",{ from: owners[0] } );
+          }
+          catch(err){
+              const errM = "caller is not owner"
+              expect(err).to.equal(errM)
+              return;
+          }
   });
-
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
