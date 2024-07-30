c388b43f1ef8d267b350e78ac3b8e327c917b46c
dro20dayethereumchallenge <dro20dayethereumchallenge@gmail.com>
2024-07-30 13:02:08 -0400

    Update Hello.js

--- test/Hello.js
+++ test/Hello.js
@@ -87,53 +87,19 @@ return;
 });
 
 
-//	  expect(contractAddress, "The current owner");
-  //        const [deployer] = await ethers.getSigners();
-    //      const owners = await ethers.getSigners();
-          //expect(deployer).to.equal(owner);
-      //    expect(deployer,"The current deployer");
-        //  expect(deployer).to.equal(deployer);
-//          expect(deployer.address).to.equal(hardhatHello.runner.address);
-
-//  });
-//});
 
 
 
 
 
 
-/// more work to do!
-//
-//
-//contract("Greeter: update greeting", (accounts) => {
-//  describe("setGreeting(string)", () => {
-//    describe("when message is sent by the owner", () => {
-//      it("sets greeting to passed in string", async () => {
-//        const greeter = await GreeterContract.deployed()
-//        const expected = "The owner changed the message";
-//
-//        await greeter.setGreeting(expected);
-//        const actual = await greeter.greet();
-//
-//        assert.equal(actual, expected, "greeting updated");
-//      });
-//    });
 
-//    describe("when message is sent by another account", () => {
-//      it("does not set the greeting", async () => {
-//        const greeter = await GreeterContract.deployed()
-//        const expected = await greeter.greet();
 
-//        try {
-//          await greeter.setGreeting("Not the owner", { from: accounts[1] });
-//        } catch(err) {
-//          const errorMessage = "Ownable: caller is not the owner"
-//          assert.equal(err.reason, errorMessage, "greeting should not update");
-//          return;
-//        }
-//        assert(false, "greeting should not update");
-//      });
-//    });
-//  });
-//});
+
+
+
+
+
+
+
+
